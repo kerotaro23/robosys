97501394e0edd06ae5d3653f2785dfed8d5bb028
kero <kerot1218@gmail.com>
2026-09-14 18:56:04 +0900

    Add test function

--- test.bash
+++ test.bash
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2026 Kero
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
 
